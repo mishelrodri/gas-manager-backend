0209b25043571156121fbc1d7be08de5ce54418f
mishelrodri <rp18027@ues.edu.sv>
2025-10-11 21:56:26 -0600

    Fix error zona horaria

--- src/main/java/com/mishelrodri/dto/VentaDTO.java
+++ src/main/java/com/mishelrodri/dto/VentaDTO.java
@@ -1,5 +1,6 @@
 package com.mishelrodri.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mishelrodri.entities.TipoTransaccion;
 import lombok.Builder;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Builder
 public record VentaDTO(
         Long id,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", timezone = "America/El_Salvador")
         LocalDateTime fecha,
         String numeroReferencia,
         String tipoTransaccion,
